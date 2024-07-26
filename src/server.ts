3d411352f9ea4908aee9057623d3905ed54c1a36
Alessandro0003 <alessandroquinta3@gmail.com>
2024-07-25 23:29:23 -0300

    🔥feat: add new routes getParticipants and getTripDetails and getParticipantDetails

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,11 @@ import { createActivity } from './routes/create-activity';
 import { getActivities } from './routes/get-activities';
 import { createLink } from './routes/create-link';
 import { getLinks } from './routes/get-links';
-import { getParticipant } from './routes/get-participant';
+import { getParticipants } from './routes/get-participants';
 import { createInvite } from './routes/create-invite';
 import { updateTrip } from './routes/update-trip';
+import { getTripDetails } from './routes/get-trip-details';
+import { getParticipantsDetails } from './routes/get-participant-details';
 
 const app = fastify()
 
@@ -28,9 +30,11 @@ app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
 app.register(getLinks)
-app.register(getParticipant)
+app.register(getParticipants)
 app.register(createInvite)
 app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipantsDetails)
 
 
 
